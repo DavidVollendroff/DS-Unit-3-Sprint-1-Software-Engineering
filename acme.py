7a7cf6b0fea5c886fb234b38543eaca75f718eec
David <civilianemail@gmail.com>
2020-01-17 08:13:06 -0800

    Create acme.py

---
+++ acme.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+class Product:
+    """Creates a named product with some characteristics"""
+    def __init__(self, name, price=10, weight=20, flammability=0.5):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = randint(1000000, 9999999)
